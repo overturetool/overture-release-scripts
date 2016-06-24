10afe5db2c53e41c1b0342c9bf89af5a41c2c145
Kenneth Lausdahl <lausdahl@eng.au.dk>
2016-06-24 20:06:34 +0200

    use full path to git ssh

--- git-set-private-key.sh
+++ git-set-private-key.sh
@@ -7,5 +7,7 @@
 echo 'ssh -i '$1' -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $*' > ssh
 chmod +x ssh
 #export GIT_TRACE=1 
-echo "export GIT_SSH=./ssh"
+SSH_PATH=`readlink -f ./ssh`
+
+echo "export GIT_SSH=${SSH_PATH}"
 
