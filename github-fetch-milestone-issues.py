4e06ee5c8134dd6472eff20be0d6f77dc111a98c
Peter W. V. Tran-Jørgensen <pvj@eng.au.dk>
2016-10-06 14:50:50 +0200

    Make github-milestone script more generic

--- github-fetch-milestone-issues.py
+++ github-fetch-milestone-issues.py
@@ -1,12 +1,21 @@
 #!/usr/bin/env python3
 
-import json
-import requests
-import os.path
+import csv
 import datetime
+import os
+import os.path
+import requests
 
-oauth_token=os.getenv("GITHUB_API_TOKEN")
-header={'Accept': 'application/vnd.github.full+json','Authorization': 'token '+oauth_token}
+
+oauth_token = os.getenv("GITHUB_API_TOKEN")
+header = {
+    'Accept': 'application/vnd.github.full+json',
+    'Authorization': 'token ' + oauth_token
+}
+
+TITLE = "Overture"
+REPO_NAME = "overture"
+REPO_OWNER = "overturetool"
 
 
 def write_issues(response):
@@ -14,50 +23,52 @@ def write_issues(response):
     if not r.status_code == 200:
         raise Exception(r.status_code)
     for issue in r.json():
-        print(issue);
+        print(issue)
         labels = issue['labels']
         for label in labels:
             if label['name'] == "Client Requested":
-                csvout.writerow([issue['number'], issue['title'].encode('utf-8'), issue['body'].encode('utf-8'), issue['created_at'], issue['updated_at']])
+                csvout.writerow(
+                    [issue['number'], issue['title'].encode('utf-8'),
+                     issue['body'].encode('utf-8'), issue['created_at'],
+                     issue['updated_at']])
 
-def write(string):
-    print(string)
 
-def writeMilestone(file,title,url,dueon,template):
-    if file == None:
+def writeMilestone(file, title, url, dueon, template):
+    if file is None:
         return
-    
-    t =title+" - Release Notes"
-    
+
+    t = title + " - Release Notes"
+
     if dueon:
         d = datetime.datetime.strptime(dueon, "%Y-%m-%dT%H:%M:%SZ")
         t += " - {:%d %B %Y}".format(d)
 
-    write(t)
-    write("Issues")
+    print(t)
+    print("Issues")
 
-    file.write("\n# ["+ t+"]("+url+")\n\n")
-    if template != None:
+    file.write("\n# [" + t + "](" + url + ")\n\n")
+    if template is not None:
         with open(template) as f:
             for line in f:
                 file.write(line)
     file.write("## Bugfixes\n\n")
-    file.write("Please note that the interactive list is at <"+url+">\n")
-    
+    file.write("Please note that the interactive list is at <" + url + ">\n")
 
-def writeIssue(file,state,number, title,url):
-    if file == None:
+
+def writeIssue(file, state, number, title, url):
+    if file is None:
         return
-    
-    write(state + " - " + title)
-    file.write("* [#" +str(number)+" "+ state + " - "+title+"]("+url+")\n")
 
-r = requests.get("http://api.github.com/repos/overturetool/overture/milestones?state=closed", headers=header)
+    print(state + " - " + title)
+    file.write("* [#" + str(number) + " " + state + " - " + title + "](" + url +
+               ")\n")
 
-file = open("output.txt", "w")
+r = requests.get(
+    "http://api.github.com/repos/" + REPO_OWNER + "/" + REPO_NAME + "/milestones?state=closed",
+    headers=header)
 
 if not r.status_code == 200:
-        raise Exception(r.status_code)
+    raise Exception(r.status_code)
 
 for milestone in r.json():
 
@@ -65,39 +76,24 @@ for milestone in r.json():
     if version.startswith('v'):
         version = version[1:]
 
-    mdname = "ReleaseNotes_"+version+".md"
-    mdnameabb = "ReleaseNotes_"+version+"_abbrev.md"
+    mdname = "ReleaseNotes_" + version + ".md"
     fileM = None
-    fileMabb = None
     if os.path.isfile(mdname):
         fileM = None
         continue
     else:
         fileM = open(mdname, "w")
-        writeMilestone(fileM,"Overture "+version,milestone['html_url'],milestone['due_on'],"ReleaseNotes-template.md")
+        writeMilestone(fileM, TITLE + " " + version, milestone['html_url'],
+                       milestone['due_on'], "ReleaseNotes-template.md")
 
-
-    if os.path.isfile(mdnameabb):
-        fileMabb = None
-        
-    else:
-        fileMabb = open(mdnameabb, "w")
-        writeMilestone(fileMabb,"Overture "+version,milestone['html_url'],milestone['due_on'],"ReleaseNotes-template-abbrev.md")
-        
-    writeMilestone(file,version,milestone['url'],milestone['due_on'],None)
-
-
-    ri = requests.get("http://api.github.com/repos/overturetool/overture/issues?state=all&milestone="+str(milestone['number']), headers=header)
+    ri = requests.get(
+        "http://api.github.com/repos/" + REPO_OWNER + "/" + REPO_NAME + "/issues?state=all&milestone=" +
+        str(milestone['number']),
+        headers=header)
     if not ri.status_code == 200:
         raise Exception(ri.status_code)
     for issue in ri.json():
-        writeIssue(file,issue['state'],issue['number'], issue['title'],issue['html_url'])
-        writeIssue(fileM,issue['state'],issue['number'], issue['title'],issue['html_url'])
-        if fileMabb != None:
-            writeIssue(fileMabb,issue['state'],issue['number'], issue['title'],issue['html_url'])
-    if fileM !=None:
+        writeIssue(fileM, issue['state'], issue['number'], issue['title'],
+                   issue['html_url'])
+    if fileM is not None:
         fileM.close()
-    if fileMabb !=None:
-        fileMabb.close()
-
-file.close()
